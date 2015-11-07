f9b7bb0e4c80fb8e098cae47bb37e93053624932
samuelohagan <samohagan.ohagan@gmail.com>
2015-11-06 20:27:50 -0800

    Revert "Revert "initialcommit""
    This reverts commit c3518f3a02815e783e3a0cbbc8f1c8306714c73b.

---
+++ montecarloform/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace montecarloform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.myTextBox = new System.Windows.Forms.TextBox();
+            this.myTextBox2 = new System.Windows.Forms.TextBox();
+            this.myTextBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label90 = new System.Windows.Forms.Label();
+            this.label75 = new System.Windows.Forms.Label();
+            this.label50 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(2, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(911, 597);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 638);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // myTextBox
+            // 
+            this.myTextBox.Location = new System.Drawing.Point(46, 676);
+            this.myTextBox.Name = "myTextBox";
+            this.myTextBox.Size = new System.Drawing.Size(151, 20);
+            this.myTextBox.TabIndex = 2;
+            this.myTextBox.Text = "Starting Price";
+            // 
+            // myTextBox2
+            // 
+            this.myTextBox2.Location = new System.Drawing.Point(575, 667);
+            this.myTextBox2.Name = "myTextBox2";
+            this.myTextBox2.Size = new System.Drawing.Size(129, 20);
+            this.myTextBox2.TabIndex = 3;
+            this.myTextBox2.Text = "Expected Price Increase";
+            // 
+            // myTextBox3
+            // 
+            this.myTextBox3.Location = new System.Drawing.Point(746, 667);
+            this.myTextBox3.Name = "myTextBox3";
+            this.myTextBox3.Size = new System.Drawing.Size(100, 20);
+            this.myTextBox3.TabIndex = 4;
+            this.myTextBox3.Text = "StdDeviation";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(63, 745);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "90% chance that the price will be greater then this";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(486, 745);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(360, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "75% chance that the price will be greater then this";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(63, 826);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(360, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "50% chance that the price will be greater then this";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(486, 826);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(360, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "25% chance that the price will be greater then this";
+            // 
+            // label90
+            // 
+            this.label90.AutoSize = true;
+            this.label90.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label90.Location = new System.Drawing.Point(155, 789);
+            this.label90.Name = "label90";
+            this.label90.Size = new System.Drawing.Size(42, 18);
+            this.label90.TabIndex = 9;
+            this.label90.Text = "Price";
+            // 
+            // label75
+            // 
+            this.label75.AutoSize = true;
+            this.label75.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label75.Location = new System.Drawing.Point(662, 789);
+            this.label75.Name = "label75";
+            this.label75.Size = new System.Drawing.Size(42, 18);
+            this.label75.TabIndex = 10;
+            this.label75.Text = "Price";
+            // 
+            // label50
+            // 
+            this.label50.AutoSize = true;
+            this.label50.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label50.Location = new System.Drawing.Point(155, 875);
+            this.label50.Name = "label50";
+            this.label50.Size = new System.Drawing.Size(42, 18);
+            this.label50.TabIndex = 11;
+            this.label50.Text = "Price";
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label25.Location = new System.Drawing.Point(662, 875);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(42, 18);
+            this.label25.TabIndex = 12;
+            this.label25.Text = "Price";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 920);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.label50);
+            this.Controls.Add(this.label75);
+            this.Controls.Add(this.label90);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.myTextBox3);
+            this.Controls.Add(this.myTextBox2);
+            this.Controls.Add(this.myTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Monte Carlo Stock Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox myTextBox;
+        private System.Windows.Forms.TextBox myTextBox2;
+        private System.Windows.Forms.TextBox myTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label90;
+        private System.Windows.Forms.Label label75;
+        private System.Windows.Forms.Label label50;
+        private System.Windows.Forms.Label label25;
+    }
+}
+
---
+++ montecarloform/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Meta.Numerics;
+using Meta.Numerics.Statistics.Distributions;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Diagnostics;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace montecarloform
+{
+
+    public partial class 
+        
+        
+        Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+            
+
+        }
+        public static double Percentile(IEnumerable<double> seq, double percentile)
+        {
+            var elements = seq.ToArray();
+            Array.Sort(elements);
+            double realIndex = percentile * (elements.Length - 1);
+            int index = (int)realIndex;
+            double frac = realIndex - index;
+            if (index + 1 < elements.Length)
+                return elements[index] * (1 - frac) + elements[index + 1] * frac;
+            else
+                return elements[index];
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+
+      
+            double[] ab = new double[30];
+            double[] page = new double[30];
+            double[] sabc = new double[30];
+            double[] derp = new double[31];
+            
+            Random rnd = new Random();
+            double[] randomdouble = new double[30];
+            Series series = this.chart1.Series.Add("a");
+            chart1.Series["a"].ChartType = SeriesChartType.Line;
+            for (int i = 0; i < page.Length; ++i)
+            {
+                page[i] = rnd.Next(101, 9999);
+                randomdouble[i] = (double)page[i] / (double)10000;
+                double mean = Convert.ToDouble(myTextBox2.Text);
+                double stdev = Convert.ToDouble(myTextBox3.Text);
+                Distribution n = new NormalDistribution(mean, stdev);
+                double b = n.InverseLeftProbability(randomdouble[i]);
+                double x = ((double)b / (double)100) + 1;
+                
+                sabc[i] = x;
+                derp[0] = Convert.ToDouble(myTextBox.Text);
+
+                derp[i + 1] = (double)x * (double)derp[i];                                             
+                Console.WriteLine(x);
+                // Add point.
+                series.Points.Add(derp[i]);
+            }
+            
+            double[] k = new double[1001];
+            for (int deh = 0; deh < k.Length; ++deh)
+            {
+                for (int i = 0; i < page.Length; ++i)
+                {
+                    page[i] = rnd.Next(101, 9999);
+                    randomdouble[i] = (double)page[i] / (double)10000;
+                    double mean = Convert.ToDouble(myTextBox2.Text);
+                    double stdev = Convert.ToDouble(myTextBox3.Text);
+                    Distribution n = new NormalDistribution(mean, stdev);
+                    double b = n.InverseLeftProbability(randomdouble[i]);
+                    double x = ((double)b / (double)100) + 1;
+
+                    sabc[i] = x;
+                    derp[0] = Convert.ToDouble(myTextBox.Text);
+
+                    derp[i + 1] = (double)x * (double)derp[i];
+                    
+                    // Add point.
+                    
+                }
+                k[deh] = derp[29];
+                //Console.WriteLine(k[deh]);
+                
+
+
+
+
+             }
+            //90% chance that the price will be greater then this
+            label90.Text = Convert.ToString(Percentile(k, 0.10));
+            //75% chance that the price will be greater then this
+            label75.Text = Convert.ToString(Percentile(k, 0.25));
+            //50% chance that the price will be greater then this
+            label50.Text = Convert.ToString(Percentile(k, 0.50));
+            //25% chance that the price will be greater then this
+            label25.Text = Convert.ToString(Percentile(k, 0.75));
+
+        }
+        public void ResetForm()
+        {
+            Random rnd = new Random();
+            chart1.Series.Clear();
+           
+
+
+
+            
+        }
+    }
+}
